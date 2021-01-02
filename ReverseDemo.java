ec8e70af8c882662286e5ed62736cc4f4d65696d
firozkabirroz <firozkabirroz@gmail.com>
2021-01-02 21:53:41 +0600

    Java practice code added

---
+++ ReverseDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseDemo{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int num,sum=0,temp,r;
+		System.out.println("Enter a number: ");
+		num = input.nextInt();
+		
+		temp = num;
+		while(temp !=0){
+			r = temp %10;
+			sum= sum *10 +r;
+			temp = temp/10;
+		}
+		System.out.println("Reverse Number is : " +sum);
+	}
+}
